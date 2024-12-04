0d7655ddfbeedb0bae99e04bb7db4626e687f5d7
Bradley Axen <baxen@squareup.com>
2024-12-04 14:33:42 -0800

    reword

--- ui/desktop/src/components/WelcomeScreen.tsx
+++ ui/desktop/src/components/WelcomeScreen.tsx
@@ -18,19 +18,11 @@ export function WelcomeScreen({ className, onDismiss }: WelcomeScreenProps) {
         <div className="whitespace-pre-wrap text-gray-600">
           Goose is your AI-powered agent.
           <br /><br />
-          Type your question or command in the input box or click on one of the examples!
-          <br />
-      You will have to occasionally log in to authenticate, go ahead and use SSO in the browser popup.
-          <br />
-          <br />
-          Try ⌘+N and ⌘+O
-          <br /><br />
           <b> Warning: During the beta, your chats are not saved - closing the window <br />
               or closing the app will lose your history. <br />
           </b>
-          Support for saving chats will come before the full 1.0 release
-
-
+          <br /><br />
+          Try ⌘+N for a new window, or ⌘+O to work on a specific directory.
         </div>
 
         <button
