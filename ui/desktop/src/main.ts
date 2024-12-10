f7d02506f04a2dcfa228d138a3f9dbc964d86d96
Mic Neale <michael.neale@gmail.com>
2024-12-10 13:26:53 +1100

    unused import

--- ui/desktop/src/main.ts
+++ ui/desktop/src/main.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { loadZshEnv } from './utils/loadEnv';
-import { app, BrowserWindow, Tray, Menu, globalShortcut, ipcMain, Notification, MenuItem, dialog, shell } from 'electron';
+import { app, BrowserWindow, Tray, Menu, globalShortcut, ipcMain, Notification, MenuItem, dialog } from 'electron';
 import path from 'node:path';
 import { startGoosed } from './goosed';
 import started from "electron-squirrel-startup";
