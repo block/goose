42b96538656956cf61447280d1344c4ebdbeb832
Adewale Abati <acekyd01@gmail.com>
2025-01-23 20:11:10 +0100

    final final relative path fix

--- extensions-site/vite.config.ts
+++ extensions-site/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: '/goose/v1/extensions', 
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
