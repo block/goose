3774eca9005b10328936d61ba589625c6f6c02b9
genesis0000 <genesis0000@bvm.network>
2025-02-04 13:45:50 +0700

    - feat: eternalai - Onchain Agent Transformer - transform any agents into unstoppable Solidity smart contracts deployed on 10+ blockchains Onchain Agent Transformer: transform any agents into unstoppable Solidity smart contracts deployed on 10+ blockchains

--- crates/goose/examples/agent_eternalai.rs
+++ crates/goose/examples/agent_eternalai.rs
@@ -2,7 +2,8 @@ use dotenv::dotenv;
 use futures::StreamExt;
 use goose::agents::AgentFactory;
 use goose::message::Message;
-use goose::providers::eternalai::EternalAiProvider;
+use goose::model::ModelConfig;
+use goose::providers::eternalai::{EternalAiProvider, ETERNAL_AI_DEFAULT_MODEL};
 
 #[tokio::main]
 async fn main() {
@@ -11,10 +12,16 @@ async fn main() {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         .init();
-    let provider = Box::new(EternalAiProvider::default());
+    let model = ModelConfig::new(ETERNAL_AI_DEFAULT_MODEL.parse().unwrap());
+    let provider_result = EternalAiProvider::from_env(model, Some("8453".to_string()));
+
+    let provider = match provider_result {
+        Ok(provider) => provider,
+        Err(e) => panic!("Failed to create provider: {}", e),
+    };
 
     // Setup an agent
-    let agent = AgentFactory::create("reference", provider).expect("default should exist");
+    let agent = AgentFactory::create("reference", Box::new(provider)).expect("default should exist");
 
     // println!("Extensions:");
     for extension in agent.list_extensions().await {
