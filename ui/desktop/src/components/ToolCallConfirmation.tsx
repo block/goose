6bc1569a0a8d743b312c264a0d458437a5fd18f9
Lily Delalande <ldelalande@squareup.com>
2025-02-28 18:52:18 -0500

    button colors

--- ui/desktop/src/components/ToolCallConfirmation.tsx
+++ ui/desktop/src/components/ToolCallConfirmation.tsx
@@ -17,13 +17,9 @@ export default function ToolConfirmation({ toolConfirmationId }) {
       </div>
       <div className="goose-message-tool bg-bgApp border border-borderSubtle dark:border-gray-700 rounded-b-2xl px-4 pt-4 pb-2 flex gap-4 mt-1">
         <button
-          className={`${
-            hovered
-              ? 'bg-white text-black dark:bg-black dark:text-white dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-800 border'
-              : 'bg-gray-100 dark:bg-gray-800'
-          } border-gray-300 rounded-full px-6 py-2 transition ${
-            disabled ? 'opacity-50 cursor-not-allowed' : ''
-          }`}
+          className={
+            'bg-black text-white dark:bg-white dark:text-black rounded-full px-6 py-2 transition'
+          }
           onMouseEnter={() => setHovered(true)}
           onClick={() => handleButtonClick(true)}
           disabled={disabled}
@@ -31,9 +27,9 @@ export default function ToolConfirmation({ toolConfirmationId }) {
           Allow tool
         </button>
         <button
-          className={`bg-white text-black dark:bg-black dark:text-white border border-gray-300 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-full px-6 py-2 transition ${
-            disabled ? 'opacity-50 cursor-not-allowed' : ''
-          }`}
+          className={
+            'bg-white text-black dark:bg-black dark:text-white border border-gray-300 dark:border-gray-700 rounded-full px-6 py-2 transition'
+          }
           onClick={() => handleButtonClick(false)}
           disabled={disabled}
         >
