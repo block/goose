a73a272987cb20c68f609c6e0772b5e0320065f0
Lily Delalande <ldelalande@squareup.com>
2025-02-28 09:30:06 -0500

    latest

--- ui/desktop/src/components/settings_v2/providers/modals/configuration_modal_subcomponents/ProviderLogo.tsx
+++ ui/desktop/src/components/settings_v2/providers/modals/configuration_modal_subcomponents/ProviderLogo.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+// Direct import at the top of your file
+import OpenAILogo from './icons/openai.png';
 
-export default function ProviderLogo(providerName) {
-  // TODO: Determine which logo to display
-  const logoPath = '../icons/openai.svg'; // /icons/openai.svg
-  //const fallbackLogoPath = './icons/default.svg';
+export default function ProviderLogo({ providerName }) {
   return (
     <div className="flex justify-center mb-2">
-      <div className="w-16 h-16 bg-black rounded-full flex items-center justify-center">
-        <img src={logoPath} alt={`${providerName} logo`} className="w-8 h-8" />
+      {/* Smaller circle */}
+      <div className="w-12 h-12 bg-black rounded-full flex items-center justify-center">
+        {/* Larger image */}
+        <img
+          src={OpenAILogo}
+          alt={`${providerName} logo`}
+          className="w-10 h-10" // Increased from w-8 h-8
+        />
       </div>
     </div>
   );
