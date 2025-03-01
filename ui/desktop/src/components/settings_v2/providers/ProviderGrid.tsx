d5a1e9b5a6de6d1d4b18f47dadb7bb39471ef5c0
Lily Delalande <ldelalande@squareup.com>
2025-02-28 20:53:22 -0500

    use new grid class

--- ui/desktop/src/components/settings_v2/providers/ProviderGrid.tsx
+++ ui/desktop/src/components/settings_v2/providers/ProviderGrid.tsx
@@ -7,7 +7,7 @@ import ProviderConfigurationModal from './modal/ProviderConfiguationModal';
 
 function GridLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-3 auto-rows-fr max-w-full [&_*]:z-20">
+    <div className="grid grid-cols-[repeat(auto-fill,_minmax(140px,_1fr))] gap-3 [&_*]:z-20">
       {children}
     </div>
   );
