8760f7166c9a0d0a6a559ed70573381cec100870
Salman Mohammed <smohammed@squareup.com>
2025-03-10 11:57:51 -0400

    Update anthropic.rs

--- crates/goose/src/providers/anthropic.rs
+++ crates/goose/src/providers/anthropic.rs
@@ -161,15 +161,14 @@ impl Provider for AnthropicProvider {
         headers.insert("x-api-key", self.api_key.parse().unwrap());
         headers.insert("anthropic-version", "2023-06-01".parse().unwrap());
 
-        let is_thinking_enabled = std::env::var("ANTHROPIC_THINKING_ENABLED").is_ok();
-        if self.model.model_name.starts_with("claude-3-7-sonnet-") && is_thinking_enabled {
+        if self.model.model_name.starts_with("claude-3-7-sonnet-") {
+            // https://docs.anthropic.com/en/docs/build-with-claude/extended-thinking#extended-output-capabilities-beta
+            headers.insert("anthropic-beta", "output-128k-2025-02-19".parse().unwrap());
             // https://docs.anthropic.com/en/docs/build-with-claude/tool-use/token-efficient-tool-use
             headers.insert(
                 "anthropic-beta",
                 "token-efficient-tools-2025-02-19".parse().unwrap(),
-            );
-            // https://docs.anthropic.com/en/docs/build-with-claude/extended-thinking#extended-output-capabilities-beta
-            headers.insert("anthropic-beta", "output-128k-2025-02-19".parse().unwrap());
+            );            
         }
         // Make request
         let response = self.post(headers, payload.clone()).await?;
