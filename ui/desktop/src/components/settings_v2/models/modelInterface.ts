210deaf70eb30f0b333dd8cf631027054a575dcb
Lily Delalande <ldelalande@squareup.com>
2025-03-31 09:55:52 -0400

    set up model list component

---
+++ ui/desktop/src/components/settings_v2/models/modelInterface.ts
@@ -0,0 +1,8 @@
+export default interface Model {
+  id?: number; // Make `id` optional to allow user-defined models
+  name: string;
+  provider: string;
+  lastUsed?: string;
+  alias?: string; // optional model display name
+  subtext?: string; // goes below model name if not the provider
+}
