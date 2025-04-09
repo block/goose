e7eaa05769e3f278872f1a247408b74ce7341fea
Zane Staggs <zane@squareup.com>
2025-04-09 07:57:47 -0700

    removed theme context not needed at this point will revisit later

--- ui/desktop/src/renderer.tsx
+++ ui/desktop/src/renderer.tsx
@@ -5,23 +5,20 @@ import { ModelProvider } from './components/settings/models/ModelContext';
 import { ConfigProvider } from './components/ConfigContext';
 import { ErrorBoundary } from './components/ErrorBoundary';
 import { ActiveKeysProvider } from './components/settings/api_keys/ActiveKeysContext';
-import { ThemeProvider } from './components/ThemeContext';
 import { patchConsoleLogging } from './utils';
 
 patchConsoleLogging();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <ConfigProvider>
-        <ModelProvider>
-          <ActiveKeysProvider>
-            <ErrorBoundary>
-              <App />
-            </ErrorBoundary>
-          </ActiveKeysProvider>
-        </ModelProvider>
-      </ConfigProvider>
-    </ThemeProvider>
+    <ConfigProvider>
+      <ModelProvider>
+        <ActiveKeysProvider>
+          <ErrorBoundary>
+            <App />
+          </ErrorBoundary>
+        </ActiveKeysProvider>
+      </ModelProvider>
+    </ConfigProvider>
   </React.StrictMode>
 );
