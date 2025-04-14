42ea154eaea6cf347f9fb7711e7b225fdfe4b904
Yingjie He <yingjiehe@squareup.com>
2025-04-14 16:50:32 -0700

    feat: init UI implementation for tool permission control

--- ui/desktop/src/components/settings_v2/SettingsView.tsx
+++ ui/desktop/src/components/settings_v2/SettingsView.tsx
@@ -44,7 +44,7 @@ export default function SettingsView({
                 showEnvVars={viewOptions.showEnvVars}
               />
               {/* Goose Modes */}
-              <ModeSection />
+              <ModeSection setView={setView} />
               {/*Session sharing*/}
               <SessionSharingSection />
             </div>
