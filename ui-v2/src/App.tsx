83fafc1e948530233dee5453a705d1c6863e15c9
Zane Staggs <zane@squareup.com>
2025-05-20 17:48:20 -0700

    add title bar, improve fonts and tailwind setup

--- ui-v2/src/App.tsx
+++ ui-v2/src/App.tsx
@@ -6,9 +6,17 @@ import SuspenseLoader from './components/SuspenseLoader';
 
 const App: React.FC = (): React.ReactElement => {
   return (
-    <Suspense fallback={<SuspenseLoader />}>
-      <Outlet />
-    </Suspense>
+    <div>
+      <div className="titlebar-drag-region" />
+      <div className="relative w-screen h-screen overflow-hidden bg-bgApp flex flex-col">
+        <div className="relative flex items-center h-14 border-b border-borderSubtle w-full" />
+        <div className="pt-14">
+          <Suspense fallback={<SuspenseLoader />}>
+            <Outlet />
+          </Suspense>
+        </div>
+      </div>
+    </div>
   );
 };
 
