3015ae37832a0ad2e2606ad913236758fc534b85
Rizel Scarlett <rizel@squareup.com>
2025-06-18 14:04:47 -0400

    singular button

--- documentation/src/components/WindowsDesktopInstallButtons.js
+++ documentation/src/components/WindowsDesktopInstallButtons.js
@@ -4,7 +4,7 @@ import { IconDownload } from "@site/src/components/icons/download";
 const WindowsDesktopInstallButtons = () => {
   return (
     <div>
-      <p>To download Goose Desktop for Windows, click the buttons below:</p>
+      <p>To download Goose Desktop for Windows, click the button below:</p>
       <div className="pill-button">
         <Link
           className="button button--primary button--lg"
