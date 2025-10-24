c6a75f3d8a497795fa30df119666be53d4cbf6fb
Jack Amadeo <jackamadeo@squareup.com>
2025-10-24 14:55:13 -0400

    expand tilde

--- crates/goose/src/config/search_path.rs
+++ crates/goose/src/config/search_path.rs
@@ -3,19 +3,23 @@ use std::{env, ffi::OsString, path::PathBuf};
 use crate::config::{Config, ConfigError};
 
 pub fn search_path_var() -> Result<OsString, ConfigError> {
-    let mut paths: Vec<_> = Config::global()
+    let paths = Config::global()
         .get_goose_search_paths()
         .or_else(|err| match err {
             ConfigError::NotFound(_) => Ok(vec![]),
             err => Err(err),
         })?
         .into_iter()
-        .map(PathBuf::from)
-        .collect();
+        .map(|s| PathBuf::from(shellexpand::tilde(&s).as_ref()));
 
-    if let Some(existing) = env::var_os("PATH") {
-        paths.extend(env::split_paths(&existing));
-    }
-
-    env::join_paths(paths).map_err(|e| ConfigError::DeserializeError(format!("{}", e)))
+    env::join_paths(
+        paths.chain(
+            env::var_os("PATH")
+                .as_ref()
+                .map(env::split_paths)
+                .into_iter()
+                .flatten(),
+        ),
+    )
+    .map_err(|e| ConfigError::DeserializeError(format!("{}", e)))
 }
