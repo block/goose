ed56e167ec54488d565e4ab4d1af89233006c9cf
Michael Neale <michael.neale@gmail.com>
2025-11-07 16:57:23 +1100

    need this to give it a second to start

--- crates/goose-server/src/commands/agent.rs
+++ crates/goose-server/src/commands/agent.rs
@@ -67,7 +67,7 @@ pub async fn run() -> Result<()> {
 
     let app_state_clone = app_state.clone();
     tokio::spawn(async move {
-        //tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+        tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
         app_state_clone.auto_start_tunnel(port).await;
     });
 
