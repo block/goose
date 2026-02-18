5e787655e244b9b35f457324aa2e71313ec65e37
Alex Hancock <alexhancock@block.xyz>
2026-02-18 11:38:36 -0500

    feat: goose-acp powered TUI

---
+++ ui/acp/src/index.ts
@@ -0,0 +1,10 @@
+export { createHttpStream } from "./transport.js";
+
+export {
+  ClientSideConnection,
+  type AnyMessage,
+  type Stream,
+  type SessionNotification,
+  type RequestPermissionRequest,
+  type RequestPermissionResponse,
+} from "@agentclientprotocol/sdk";
