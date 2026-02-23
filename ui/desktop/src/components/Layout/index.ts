8f5f9adaa3180e963342a9adf35627542d704bf8
Zane Staggs <zane@squareup.com>
2026-02-23 08:41:38 -0800

    remove unused barrel import files

--- ui/desktop/src/components/Layout/index.ts
+++
@@ -1,4 +0,0 @@
-export { AppLayout } from './AppLayout';
-export { NavigationProvider, useNavigationContext, useNavigationContextSafe } from './NavigationContext';
-export type { NavigationMode, NavigationStyle, NavigationPosition, NavigationPreferences } from './NavigationContext';
-export { Navigation } from './NavigationPanel';
--- ui/desktop/src/components/settings/app/index.ts
+++
@@ -1,4 +0,0 @@
-export { NavigationModeSelector } from './NavigationModeSelector';
-export { NavigationStyleSelector } from './NavigationStyleSelector';
-export { NavigationPositionSelector } from './NavigationPositionSelector';
-export { NavigationCustomizationSettings } from './NavigationCustomizationSettings';
