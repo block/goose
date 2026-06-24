b14f12d10a361721aaca1ffd444edc4612bc690d
Lifei Zhou <lifei@squareup.com>
2026-06-24 14:09:29 +1000

    removed the too many tools warning

--- ui/desktop/src/components/BaseChat.tsx
+++ ui/desktop/src/components/BaseChat.tsx
@@ -26,7 +26,6 @@ import { scanRecipe } from '../recipe';
 import type { Recipe } from '../recipe';
 import { UserInput } from '../types/message';
 import RecipeActivities from './recipes/RecipeActivities';
-import { useToolCount } from './alerts/useToolCount';
 import { getThinkingMessage, getTextAndImageContent } from '../types/message';
 import ParameterInputModal from './ParameterInputModal';
 import { substituteParameters } from '../utils/parameterSubstitution';
@@ -272,8 +271,6 @@ export default function BaseChat({
       }
     }
   }, [messages.length]);
-
-  const toolCount = useToolCount(sessionId);
 
   // Listen for global scroll-to-bottom requests (e.g., from MCP UI prompt actions)
   useEffect(() => {
@@ -528,7 +525,6 @@ export default function BaseChat({
             recipe={recipe}
             recipeAccepted={!hasNotAcceptedRecipe}
             initialPrompt={initialPrompt}
-            toolCount={toolCount || 0}
             sessionModel={sessionModel}
             sessionProvider={sessionProvider}
             sessionLoaded={sessionLoaded}
